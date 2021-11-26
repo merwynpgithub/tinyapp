61bcf3464a3b157a40a8891913f0f84421facec4
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-26 12:15:05 -0500

    setup app

---
+++ express_server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get('/', (req, res) => {
+  res.send("Hello");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
