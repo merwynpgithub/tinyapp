d70fa15295301e85e5b979b7f4dba6d705353275
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-28 13:10:03 -0500

    add urls new form to add new links

--- express_server.js
+++ express_server.js
@@ -9,6 +9,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
