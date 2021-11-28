82df4f2e5717cec54016f27f6939fd390e66b8ca
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-28 14:40:31 -0500

    add body parser

--- express_server.js
+++ express_server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+
+}
+generateRandomString();
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
