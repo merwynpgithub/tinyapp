760a8f76f80593d06783b4b9148eb7cad101f970
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-12-03 11:26:22 -0500

    move users and urlDatabase objects to helper file

--- express_server.js
+++ express_server.js
@@ -3,42 +3,15 @@ const app = express();
 const port = 8080;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const users = { 
-  "user1": {
-    id: "user1", 
-    email: "user1@example.com", 
-    password: "pass1"
-  },
- "user2": {
-    id: "user2", 
-    email: "user2@example.com", 
-    password: "pass2"
-  }
-};
-const urlDatabase = {
-  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "user2" },
-  i3BoGr: { longURL: "https://www.google.ca", userID: "user2" },
-  b2xVn2: {longURL: "http://www.lighthouselabs.ca", userID: "user1" },
-  psm5xK: {longURL: "http://www.google.com", userID: "user1" }
-};
-const { findUserEmail, generateRandomString } = require('./helper');
 
-//helper to sort UrlDatabase as per particular UserId
-const urlsForUser = (id) => {
-  const urlDataUser = {};
-  for (const data in urlDatabase) {
-    if (urlDatabase[data].userID === id) {
-      urlDataUser[data] = urlDatabase[data];
-    }
-  }
-  return urlDataUser;
-};
+const { users, urlDatabase, findUserEmail, generateRandomString, urlsForUser } = require('./helper');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set("view engine", "ejs");
 
-//POST
+
+//POST endpoints
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortUrlString = req.params.shortURL;
   delete urlDatabase[shortUrlString];
--- helper.js
+++ helper.js
@@ -1,3 +1,21 @@
+const users = { 
+  "user1": {
+    id: "user1", 
+    email: "user1@example.com", 
+    password: "pass1"
+  },
+ "user2": {
+    id: "user2", 
+    email: "user2@example.com", 
+    password: "pass2"
+  }
+};
+const urlDatabase = {
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "user2" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "user2" },
+  b2xVn2: {longURL: "http://www.lighthouselabs.ca", userID: "user1" },
+  psm5xK: {longURL: "http://www.google.com", userID: "user1" }
+};
 const findUserEmail = (email, users) => {
   for (const userId in users) {
     if (users[userId].email === email) {
@@ -6,14 +24,23 @@ const findUserEmail = (email, users) => {
   }
   return null;
 };
-
+//string for shortUrl
 const generateRandomString = function() {
   const date = Date.now();
   const dateStr = Math.round((date * Math.random() * 2) / Math.pow(10, 6)).toString();
   return dateStr;
   // return 'abcdef';
 };
+//helper to sort UrlDatabase as per particular UserId
+const urlsForUser = (id) => {
+  const urlDataUser = {};
+  for (const data in urlDatabase) {
+    if (urlDatabase[data].userID === id) {
+      urlDataUser[data] = urlDatabase[data];
+    }
+  }
+  return urlDataUser;
+};
 
 
-
-module.exports = { findUserEmail, generateRandomString };
+module.exports = { users, urlDatabase, findUserEmail, generateRandomString, urlsForUser };
